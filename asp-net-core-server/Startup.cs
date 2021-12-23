be033f5698b1f656b93c2d9910a3da861530db2c
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-12-23 19:14:49 +0300

    Replace DataSourceName with ConnectionName and apply BC T1018632

--- asp-net-core-server/Startup.cs
+++ asp-net-core-server/Startup.cs
@@ -33,12 +33,17 @@ namespace AspNetCoreDashboardBackend {
                 // Adds the DevExpress middleware.
                 .AddDevExpressControls()
                 // Adds controllers.
-                .AddControllers()
+                .AddControllers();
+
                 // Configures the dashboard backend.
-                .AddDefaultDashboardController(configurator => {
+                services.AddScoped<DashboardConfigurator>((IServiceProvider serviceProvider) => {
+                    DashboardConfigurator configurator = new DashboardConfigurator();
+                    
                     configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("App_Data/Dashboards").PhysicalPath));
                     configurator.SetDataSourceStorage(CreateDataSourceStorage());
                     configurator.ConfigureDataConnection += Configurator_ConfigureDataConnection;
+
+                    return configurator;
                 });
         }
 
@@ -51,29 +56,33 @@ namespace AspNetCoreDashboardBackend {
             app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints => {
                 // Maps the dashboard route.
-                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard");
+                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard", "DefaultDashboard");
                 // Requires CORS policies.
                 endpoints.MapControllers().RequireCors("CorsPolicy");
             });
         }
+
         public DataSourceInMemoryStorage CreateDataSourceStorage() {
             DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
             DashboardJsonDataSource jsonDataSourceSupport = new DashboardJsonDataSource("Support");
+            jsonDataSourceSupport.ConnectionName = "jsonSupport";
             jsonDataSourceSupport.RootElement = "Employee";
             dataSourceStorage.RegisterDataSource("jsonDataSourceSupport", jsonDataSourceSupport.SaveToXml());
             DashboardJsonDataSource jsonDataSourceCategories = new DashboardJsonDataSource("Categories");
+            jsonDataSourceCategories.ConnectionName = "jsonCategories";
             //jsonDataSourceCategories.RootElement = "";
             dataSourceStorage.RegisterDataSource("jsonDataSourceCategories", jsonDataSourceCategories.SaveToXml());
             return dataSourceStorage;
         }
+
         private void Configurator_ConfigureDataConnection(object sender, ConfigureDataConnectionWebEventArgs e) {
-            if (e.DataSourceName.Contains("Support")) {
+            if (e.ConnectionName == "jsonSupport") {
                 Uri fileUri = new Uri(FileProvider.GetFileInfo("App_Data/Support.json").PhysicalPath, UriKind.RelativeOrAbsolute);
                 JsonSourceConnectionParameters jsonParams = new JsonSourceConnectionParameters();
                 jsonParams.JsonSource = new UriJsonSource(fileUri);
                 e.ConnectionParameters = jsonParams;
             }
-            if (e.DataSourceName.Contains("Categories")) {
+            if (e.ConnectionName == "jsonCategories") {
                 Uri fileUri = new Uri(FileProvider.GetFileInfo("App_Data/Categories.json").PhysicalPath, UriKind.RelativeOrAbsolute);
                 JsonSourceConnectionParameters jsonParams = new JsonSourceConnectionParameters();
                 jsonParams.JsonSource = new UriJsonSource(fileUri);
